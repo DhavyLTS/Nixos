c586c536845747aaf81428a2334787ee48c69dd6
dhavylts <dhavy.s@proton.me>
2024-07-24 20:16:22 -0300

    feat: [home] make wallpaper link

--- home/modules/style.nix
+++ home/modules/style.nix
@@ -1,6 +1,11 @@
 { inputs, pkgs, ... } : {
 	imports = [ inputs.stylix.homeManagerModules.stylix ];
 
+	home.file.".config/hypr/wallpaper.png" = {
+		source = ../wallpaper.png;
+		recursive = true;
+	};
+
 	stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
 	stylix.cursor.package = pkgs.apple-cursor;
 	stylix.cursor.name = "macOS-BigSur-White";
