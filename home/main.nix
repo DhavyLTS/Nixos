f00e66f5951fd3ef13a49d134c754eb668a94440
dhavylts <dhavy.s@proton.me>
2024-07-24 20:18:28 -0300

    feat: [home] create nvim config link

--- home/main.nix
+++ home/main.nix
@@ -1,23 +1,22 @@
-{ pkgs, ... } : {
+{ pkgs, inputs, ... } : {
 	imports = [ ./modules/shell.nix ./modules/kitty.nix ./modules/style.nix ./modules/hyprland.nix ./modules/xremap.nix ]; 
 	nixpkgs.config.allowUnfree = true;
 
 	home.homeDirectory = "/home/tanikaze";
+	home.stateVersion = "24.11";
 	home.username = "tanikaze";
 
-	programs.home-manager.enable = true;
-	home.stateVersion = "24.11";
+	home.file.".config/nvim".source = inputs.nVim;
 
 	home.packages = with pkgs; [
 		lazygit
 		vesktop
 		neovim
 		yazi
-		gcc
-		git
 	];
 
 	programs = {
+		home-manager.enable = true;
 		firefox.enable = true;
 		btop.enable = true;
 		yazi.enable = true;
