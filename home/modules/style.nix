e75b53b93ce85ebff4ea012d338447e350d5369d
dhavylts <dhavy.s@proton.me>
2024-07-24 16:10:41 -0300

    fix: [home] wrong wallpaper path

--- home/modules/style.nix
+++ home/modules/style.nix
@@ -1,11 +1,12 @@
 { inputs, pkgs, ... } : {
 	imports = [ inputs.stylix.homeManagerModules.stylix ];
+
 	stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
 	stylix.cursor.package = pkgs.apple-cursor;
 	stylix.cursor.name = "macOS-BigSur-White";
 	stylix.cursor.size = 16;
 
-	stylix.image = ../../home/wallpaper.png;
+	stylix.image = ../wallpaper.png;
 	stylix.polarity = "dark";
 	stylix.enable = true;
 }
