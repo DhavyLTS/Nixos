7b1b0d205d1363c6db8255072f175ea831c322c3
DhavyLTS <dhavy.s@proton.me>
2024-07-24 13:25:02 -0300

    inti: [system] audio module

---
+++ system/modules/audio.nix
@@ -0,0 +1,12 @@
+{ pkgs, ...} : {
+  environment.systemPackages = with pkgs; [ pavucontrol ];
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    jack.enable = true;
+    enable = true;
+  };
+}
