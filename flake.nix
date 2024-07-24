811871f2ae34c05f2afc976b1c42648b9e07e764
dhavylts <dhavy.s@proton.me>
2024-07-24 20:16:41 -0300

    feat: add input neovim config

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
 	inputs.homeManager.url = "github:nix-community/home-manager/master";
 	inputs.homeManager.inputs.nixpkgs.follows = "nixpkgs";
 	inputs.xremap.url = "github:xremap/nix-flake";
+	inputs.nVim.url = "github:dhavylts/neovim";
 	inputs.stylix.url = "github:danth/stylix";
 
 	outputs = { nixpkgs, homeManager, ... }@inputs : let pkgs = nixpkgs.legacyPackages.${system}; system = "x86_64-linux"; in {
