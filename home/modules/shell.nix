7df10ff0c219be57082707bf68a54167427711c0
dhavylts <dhavy.s@proton.me>
2024-07-24 22:41:39 -0300

    feat: [home] edit starship style

--- home/modules/shell.nix
+++ home/modules/shell.nix
@@ -1,35 +1,41 @@
 { pkgs, config, ... } : {
-  programs.zsh = {
-    syntaxHighlighting.enable = true;
-    autosuggestion.enable = true;
-    enableCompletion = true;
-    enable = true;
+	programs.zsh = {
+		syntaxHighlighting.enable = true;
+		autosuggestion.enable = true;
+		enableCompletion = true;
+		enable = true;
 
-    shellAliases = {
-      system = "FLAKE=~/nixos nh os";
-      home = "FLAKE=~/nixos nh home";
+		shellAliases = {
+			system = "FLAKE=~/nixos nh os";
+			home = "FLAKE=~/nixos nh home";
 			vim = "${pkgs.neovim}/bin/nvim";
 			vi = "${pkgs.neovim}/bin/nvim";
-      ls = "${pkgs.eza}/bin/eza";
-    };
+			ls = "${pkgs.eza}/bin/eza";
+		};
 
-    history.size = 10000;
-    history.path = "${config.xdg.dataHome}/.zsh/history";
+		history.path = "${config.xdg.dataHome}/.zsh/history";
+		history.size = 10000;
 
-    oh-my-zsh.plugins = [ "git" ];
-    oh-my-zsh.enable = true;
-  
-  };
+		oh-my-zsh.plugins = [ "git" ];
+		oh-my-zsh.enable = true;
 
-  programs.starship.enableZshIntegration = true;
-  programs.starship.enable = true;
-  programs.starship.settings = {
-    format = " $os$directory$git_branch$package$rust\n $character";
+	};
 
-    character.success_symbol = " [󱞩](bold green)";
-    charact.error_symbol = " [󱞩](bold red)";
+	programs.starship.enableZshIntegration = true;
+	programs.starship.enable = true;
+	programs.starship.settings = {
+		format = " $os$directory$git_branch$package$rust$fill$time \n $character";
+		add_newline = true;
 
-    os.disabled = false;
-    os.symbols.NixOS = "  ";
-  };
+		character.success_symbol = " [󱞩](bold green)";
+		character.error_symbol = " [󱞩](bold red)";
+
+		fill.symbol = " ";
+
+		os.disabled = false;
+		os.symbols.NixOS = " ";
+
+		time.disabled = false;
+		time.format = "[  ](bold yellow)[$time](bold white)";
+	};
 }
