20ebb4fd065785ded28d3e2a08228d42b65f2c0c
dhavylts <dhavy.s@proton.me>
2024-07-24 22:40:03 -0300

    fix: make inputs more readable

--- flake.nix
+++ flake.nix
@@ -1,11 +1,18 @@
 {
 	description = "NixOS Configuration Flake";
 	inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-	inputs.homeManager.url = "github:nix-community/home-manager/master";
-	inputs.homeManager.inputs.nixpkgs.follows = "nixpkgs";
 	inputs.xremap.url = "github:xremap/nix-flake";
-	inputs.nVim.url = "github:dhavylts/neovim";
 	inputs.stylix.url = "github:danth/stylix";
+
+	inputs.homeManager = {
+		url = "github:nix-community/home-manager/master";
+		inputs.nixpkgs.follows = "nixpkgs";
+	};
+
+	inputs.nvim = {
+		url = "github:dhavylts/neovim";
+		flake = false;
+	};
 
 	outputs = { nixpkgs, homeManager, ... }@inputs : let pkgs = nixpkgs.legacyPackages.${system}; system = "x86_64-linux"; in {
 		nixosConfigurations."tsugumori" = nixpkgs.lib.nixosSystem {
