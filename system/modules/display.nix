76057dbd3bfbdb847b99017458f2f7967a9f35da
dhavylts <dhavy.s@proton.me>
2024-07-25 01:34:39 -0300

    feat: [system] init i3wm flavor

--- system/modules/display.nix
+++ system/modules/display.nix
@@ -3,32 +3,28 @@
 	systemd.services."getty@tty1".enable = false;
 
 	services.displayManager.autoLogin.user = "tanikaze";
+	services.displayManager.defaultSession = "none+i3";
 	services.displayManager.autoLogin.enable = true;
 	services.displayManager.sddm.enable = true;
 	services.dbus.enable = true;
 
+	environment.pathsToLink = [ "/libexec" ];
+	hardware.graphics.enable = true;
+	programs.dconf.enable = true;
+
+	services.xserver.windowManager.i3.package = pkgs.i3-gaps;
+	services.xserver.desktopManager.xterm.enable = false;
+	services.xserver.windowManager.i3.enable = true;
 	services.xserver.videoDrivers = ["amdgpu"];
 	services.xserver.xkb.layout = "br";
 	services.xserver.enable = true;
 
-	hardware.graphics.enable = true;
-
-	xdg.portal.enable = true;
 	xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
-
-	programs.hyprland.xwayland.enable = true;
-	programs.hyprland.enable = true;
-
-	environment.sessionVariables."WLR_NO_HARDWARE_CURSOR" = "1";
-	environment.sessionVariables."NIXOS_OZONE_WL" = "1";
+	xdg.portal.config.common.default = "*";
+	xdg.portal.enable = true;
 
 	environment.systemPackages = with pkgs; [
-		wl-clipboard
-		hyprcursor
-		hyprland
-		hyprshot
-		kitty
-		swww
-		wofi
+		dmenu i3status i3lock i3blocks
+		xclip maim rofi
 	];
 }
