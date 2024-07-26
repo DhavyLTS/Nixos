0bc740b6b20b2ed26cf1c018de03dd9dc342fe57
dhavylts <dhavy.s@proton.me>
2024-07-26 01:23:33 -0300

    feat: [system] insert v4l2loopback modules and options

--- system/hardware.nix
+++ system/hardware.nix
@@ -3,9 +3,12 @@
   imports = [ (modulesPath + "/installer/scan/not-detected.nix")];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" "v4l2loopback" ];
+	boot.initrd.kernelModules = [ ];
   boot.extraModulePackages = [ ];
+	boot.extraModprobeConfig = ''
+		options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+	'';
 
   fileSystems."/" = {
 		device = "/dev/disk/by-uuid/528031a3-d05b-4491-bb1b-7a56846bce07";
