25d386dbbadf61ce23711e1cb21275056f90a866
dhavylts <dhavy.s@proton.me>
2024-07-26 01:22:38 -0300

    feat: [system] setup ipv4 forward and setup iptables rules

--- system/modules/network.nix
+++ system/modules/network.nix
@@ -1,4 +1,34 @@
 { ... } : {
 	networking.networkmanager.enable = true;
 	networking.hostName = "tsugumori";
+
+	networking.firewall.enable = true;
+	networking.nat.enable = true;
+
+	networking.firewall.extraCommands = ''
+		iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+		iptables -A INPUT ! -i ppp0 -j ACCEPT
+
+		iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+		iptables -A FORWARD -i enp0s20f0u2 -o ppp0 -j ACCEPT
+		iptables -A FORWARD -i br0 -o ppp0 -j ACCEPT
+
+		iptables -t nat -A POSTROUTING -o ppp0 -j MASQUERADE
+	'';
+
+	networking.firewall.extraStopCommands = ''
+		iptables -D INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+		iptables -D INPUT ! -i ppp0 -j ACCEPT
+
+		iptables -D FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+		iptables -D FORWARD -i enp0s20f0u2 -o ppp0 -j ACCEPT
+		iptables -D FORWARD -i br0 -o ppp0 -j ACCEPT
+
+		iptables -t nat -D POSTROUTING -o ppp0 -j MASQUERADE
+	'';
+
+	boot.kernel.sysctl = {
+		"net.ipv4.conf.all.forwarding" = true;
+		"net.ipv4.ip_forward" = true;
+	};
 }
