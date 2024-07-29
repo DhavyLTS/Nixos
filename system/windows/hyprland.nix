f9cdc66d23cfd080e5fe9634d37829bff15df155
dhavylts <dhavy.s@proton.me>
2024-07-29 17:22:13 -0300

    feat: [system] hyprland window manager

---
+++ system/windows/hyprland.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... } : {
+	xdg.portal.extraPortals = with pkgs; [
+		xdg-desktop-portal-hyprland
+		xdg-desktop-portal-gtk
+	];
+
+	services.displayManager.sddm.enable = true;
+	programs.hyprland.xwayland.enable = true;
+	programs.hyprland.enable = true;
+
+	environment.sessionVariables."WLR_NO_HARDWARE_CURSOR" = "1";
+	environment.sessionVariables."NIXOS_OZONE_WL" = "1";
+
+	environment.systemPackages = with pkgs; [
+		wl-clipboard hyprcursor hyprland
+			hyprshot kitty swww wofi
+	];
+}
+
