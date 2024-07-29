fa2704f94a6d4d1192cd1af6b2b12782cfe18eee
dhavylts <dhavy.s@proton.me>
2024-07-29 17:22:55 -0300

    chors: update hardware configuration

--- system/hardware.nix
+++ system/hardware.nix
@@ -1,24 +1,20 @@
-{ config, lib, modulesPath, ... }:{
-
-  imports = [ (modulesPath + "/installer/scan/not-detected.nix")];
+{ config, lib, pkgs, modulesPath, ... } : {
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-  boot.kernelModules = [ "kvm-intel" "v4l2loopback" ];
-	boot.initrd.kernelModules = [ ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-	boot.extraModprobeConfig = ''
-		options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-	'';
 
   fileSystems."/" = {
-		device = "/dev/disk/by-uuid/528031a3-d05b-4491-bb1b-7a56846bce07";
-		fsType = "ext4";
+	device = "/dev/disk/by-uuid/528031a3-d05b-4491-bb1b-7a56846bce07";
+	fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-		device = "/dev/disk/by-uuid/FE44-5521";
-		options = [ "fmask=0022" "dmask=0022" ];
-		fsType = "vfat";
+	options = [ "fmask=0022" "dmask=0022" ];
+	device = "/dev/disk/by-uuid/FE44-5521";
+	fsType = "vfat";
   };
 
   swapDevices = [ ];
