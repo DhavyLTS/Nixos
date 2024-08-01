0c7581b73c3efea4125b0dc1cb1c6b9682413fe2
dhavylts <dhavy.s@proton.me>
2024-08-01 19:37:06 -0300

    feat: [home] set kitty background opacity to 1.0

--- home/core/kitty.nix
+++ home/core/kitty.nix
@@ -1,13 +1,14 @@
-{ pkgs, lib, inputs,... } : {
+{ pkgs, lib, ... } : {
 	programs.kitty = {
 		shellIntegration.enableZshIntegration = true;
 		font.name = lib.mkForce "CaskaydiaCove Nerd Font";
-		font.package = lib.mkForce inputs.dpkgs.caskaydiacove;
+		font.package = lib.mkForce (import ../fonts/caskaydiacove.nix { inherit pkgs; });
 		font.size = lib.mkForce 12;
 		enable = true;
 		settings = {
 			"confirm_os_window_close" = lib.mkForce "0";
-			"background_opacity" = lib.mkForce "0.9";
+			"window_padding_width" = lib.mkForce "10"; 
+			"background_opacity" = lib.mkForce "1.0";
 			"background_blur" = lib.mkForce "10";
 			"shell" = "zsh";
 		};
