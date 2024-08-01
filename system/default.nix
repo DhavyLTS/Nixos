b23d7d1f656686b922a201567f8f56b7d90b368b
dhavylts <dhavy.s@proton.me>
2024-08-01 19:45:13 -0300

    feat: [system] enable nix-ld and appimage interpreter

--- system/default.nix
+++ system/default.nix
@@ -18,14 +18,21 @@
 	programs.steam.gamescopeSession.enable = true;
 	programs.gamemode.enable = true;
 	programs.steam.enable = true;
+	programs.adb.enable = true;
 
-	programs.nh.enable = true;
+	programs.nh = {
+		enable = true;
+		clean.enable = true;
+		clean.extraArgs = "--keep-since 4d --keep 3";
+	};
+
+  programs.nix-ld.enable = true;
 
 	environment.systemPackages = with pkgs; [
 		gcc git lutris wine wine64 vulkan-tools
 		winetricks vulkan-loader vulkan-headers
 		cargo rustc clippy rustup protonup
-		protontricks
+		protontricks v4l-utils droidcam
 	];
 
 	system.stateVersion = "${vars.state_version}";
@@ -33,4 +40,13 @@
 
 	boot.loader.efi.canTouchEfiVariables = true;
 	boot.loader.systemd-boot.enable = true;
+
+	boot.binfmt.registrations.appimage = {
+		wrapInterpreterInShell = false;
+		interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+		mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+		magicOrExtension = ''\x7fELF....AI\x02'';
+		recognitionType = "magic";
+		offset = 0;
+	};
 }
