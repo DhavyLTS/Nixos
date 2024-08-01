ac852aefddba96b5798eef5e0546308d158955a1
dhavylts <dhavy.s@proton.me>
2024-08-01 19:41:50 -0300

    feat: [home] add new packages and setup LD program

--- home/default.nix
+++ home/default.nix
@@ -1,6 +1,15 @@
-{ pkgs, vars, nvim, ... } : {
+{ pkgs, vars, inputs, ... } :
+
+let
+
+	awakened-poe-trade = ( import ./pkgs/awakened-poe-trade.nix { inherit pkgs; } );
+
+in
+
+{
 	imports = [
-		./wm/${vars.flavor}.nix
+		./flavor/${vars.flavor}/main.nix
+		./core/neovim.nix
 		./core/xremap.nix
 		./core/shell.nix
 		./core/kitty.nix
@@ -9,23 +18,34 @@
 
 	nixpkgs.config.allowUnfree = true;
 
+	xdg.desktopEntries = {
+		awakened-poe-trade = {
+			name = "Awakened Poe Trade";
+			exec = "awakened-poe-trade";
+			categories = [ "Application" ];
+			terminal = false;
+		};
+	};
+
 	home.homeDirectory = "/home/${vars.username}";
 	home.stateVersion = "${vars.state_version}";
 	home.username = "${vars.username}";
 
-	home.packages =  [
-		pkgs.lazygit pkgs.vesktop
-		pkgs.yazi pkgs.spotify
-		nvim
-	];
+	home.packages = with pkgs; [
+		yazi spotify-tray stremio path-of-building
+		lutris lazygit vesktop deluge-gtk
+		wine winetricks gimp
+	] ++ [ awakened-poe-trade ];
 
 	home.sessionVariables = {
 		STEAM_EXTRA_COMPAT_TOOLS_PATH = "\${HOME}/.steam/root/compatibility.d";
+		LD_LIBRARY_PATH="${pkgs.nix-ld}/lib:$LD_LIBRARY_PATH";
 	};
 
 	programs = {
 		home-manager.enable = true;
 		firefox.enable = true;
+		lazygit.enable = true;
 		btop.enable = true;
 		yazi.enable = true;
 	};
