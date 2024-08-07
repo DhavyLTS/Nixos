44a30c9e6d21d59520fff7710ab9c323444144ce
dhavylts <dhavy.s@proton.me>
2024-08-07 12:21:00 -0300

    fix: [home] disable always to all startup entries

--- home/flavor/i3wm/startup.nix
+++ home/flavor/i3wm/startup.nix
@@ -3,22 +3,22 @@
 	 {
 			command = "xrandr --output HDMI-A-0 --mode 1920x1080";
 			notification = false;
-			always = true;
+			always = false;
 		}
 		{
-			command = "sleep 2 && ${pkgs.feh}/bin/feh --bg-fill ~/.config/wallpaper.png";
+			command = "sleep 1 && ${pkgs.feh}/bin/feh --bg-fill ~/.config/wallpaper.png";
 			notification = false;
-			always = true;
+			always = false;
 		}
 		{
 			command = "kill picom && ${pkgs.picom}/bin/picom";
 			notification = false;
-			always = true;
+			always = false;
 		}
 		{
-			command = "kill polybar && ${pkgs.polybar}/bin/polybar";
+			command = "${pkgs.polybar}/bin/polybar";
 			notification = false;
-			always = true;
+			always = false;
 		}
 	];
 }
